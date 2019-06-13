d4c4fc448adbc978bebfdac1fa41e1973e7cbb6b
Szobonya Gergő <31618132+djoke23@users.noreply.github.com>
2019-06-13 21:57:18 +0200

    Járművek és ingatlanok felület megírása.

--- project/protected/interfaces/welcome.php
+++ project/protected/interfaces/welcome.php
@@ -15,3 +15,49 @@
         <td>
              <?php echo $data['age']?>
         </td>
+</tr>
+    <tr>
+        <td>
+            Autóval rendelkezik:            
+        </td>
+        <td>
+             <?php 
+             if($data_2['count(*)'] != 0){
+             echo $data_2['count(*)'];                    
+             }
+             else{
+                 echo 'Nem rendelkezik autóval.';
+             }
+              ?>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            Motorral rendelkezik:           
+        </td>
+        <td>
+             <?php if($data_3['count(*)'] != 0){
+             echo $data_3['count(*)'];
+             }else{
+                 echo 'Nem rendelkezik motorral.';
+             }
+             ?>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            Házzal rendelkezik:           
+        </td>
+        <td>
+             <?php if($data_4['count(*)'] != 0){
+             echo $data_4['count(*)'];
+             }else{
+                 echo 'Nem rendelkezik házzal.';
+             }
+             ?>
+        </td>
+    </tr>
+</table>
+<?php else:?>
+    Nincs megjeleníthető adat!
+<?php endif?>
