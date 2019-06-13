c405a058916fa9da2ca54dc5a5723c494a1a1531
Szobonya Gergő <31618132+djoke23@users.noreply.github.com>
2019-06-13 21:56:26 +0200

    Személyes adatok felület megírása.

--- project/protected/interfaces/welcome.php
+++ project/protected/interfaces/welcome.php
@@ -1,1 +1,17 @@
-
+<h3>Személyes adatok:</h3>
+<table border="1">
+    <tr>
+        <td>
+           Név:
+        </td>
+        <td>
+           <?php echo $data['name']?>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            Kor:            
+        </td>
+        <td>
+             <?php echo $data['age']?>
+        </td>
