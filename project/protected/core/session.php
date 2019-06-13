15c265ff93c047f1ce6c9b89525132b98f9a2873
Szobonya Gergő <31618132+djoke23@users.noreply.github.com>
2019-06-13 21:53:59 +0200

    Bejelentkezési funkció megírása.

--- project/protected/core/session.php
+++ project/protected/core/session.php
@@ -2,3 +2,18 @@ function IsUserLoggedIn(){
     return  array_key_exists('uid', $_SESSION) &&
             is_numeric($_SESSION['uid']);
 }
+function Login($username, $password){
+    $password = sha1($password);
+          
+    $query = "SELECT * FROM users WHERE userName = :username AND userPsswd = :password";
+    $params = [':username' => $username, ':password' => $password];
+   
+    $userRecord = getRecord($query,$params);
+    
+    if(!empty($userRecord)){
+        $_SESSION['uid'] = $userRecord['userID'];
+        $_SESSION['uname'] = $userRecord['userName'];
+        return true;
+    }
+    return false;
+}
