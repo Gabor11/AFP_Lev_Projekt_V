713bc8821f81e220b3e9af25481f36b7970d650b
Szobonya Gergő <31618132+djoke23@users.noreply.github.com>
2019-06-13 21:39:02 +0200

    Bejelentkezési felület megírása.

--- project/protected/interfaces/login.php
+++ project/protected/interfaces/login.php
@@ -1,1 +1,7 @@
-
+<form method="POST">
+            <label>Felhasználónév:</label><br/>
+            <input type="text" name="username"/><br/>
+            <label>Jelszó:</label><br/>
+            <input type="password" name="password"/><br/>
+            <button type="submit" style="margin-top: 5px;" name="login">Bejelentkezés</button>
+</form> 
