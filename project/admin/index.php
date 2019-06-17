10de4245ebf5cec74ec641e28b03ee64c605d424
Gabor11 <gabor@valami.com>
2019-06-18 00:49:10 +0200

    menu and content added in index.php

--- project/admin/index.php
+++ project/admin/index.php
@@ -1,7 +1,8 @@
 <?php
     session_start();
 	require_once '../protected/configs/admin_config.php';
-    
+    require_once '../protected/core/session.php';
+    require_once '../protected/core/database.php';   
 ?>
 
 <html>
@@ -11,6 +12,11 @@
         </title>  
         <link href="../styles.css" type="text/css" rel="stylesheet">
     </head>
-
+    <div id="menu" class="menu" >
+        <?php require_once M_INTERFACES.'menu.php'; ?>
+    </div>
+    <div id="content" class="content">
+        <?php require_once M_INTERFACES.'content.php';?>
+    </div>
     
 </html>
