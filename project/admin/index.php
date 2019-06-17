8e0b25f376dbd33cc130ce429bfe047d3cceff2b
Gabor11 <gabor@valami.com>
2019-06-18 00:55:43 +0200

    index.php contains the main parts (menu, footer, header, content)

--- project/admin/index.php
+++ project/admin/index.php
@@ -1,8 +1,8 @@
 <?php
     session_start();
-	require_once '../protected/configs/admin_config.php';
+    require_once '../protected/configs/admin_config.php';
     require_once '../protected/core/session.php';
-    require_once '../protected/core/database.php';   
+    require_once '../protected/core/database.php';        
 ?>
 
 <html>
@@ -12,8 +12,8 @@
         </title>  
         <link href="../styles.css" type="text/css" rel="stylesheet">
     </head>
-	<div id="header" class="l_header">
-
+    <div id="header" class="l_header">
+        <?php require_once M_INTERFACES.'header.php';?>
     </div>
     <div id="menu" class="menu" >
         <?php require_once M_INTERFACES.'menu.php'; ?>
@@ -21,9 +21,9 @@
     <div id="content" class="content">
         <?php require_once M_INTERFACES.'content.php';?>
     </div>
-	
-	<div id="footer" class="a_footer">
-
+    
+    <div id="footer" class="a_footer">
+        <?php require_once M_INTERFACES.'footer.php';?>
     </div>
     
 </html>
