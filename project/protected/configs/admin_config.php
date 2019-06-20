31784a33b3e56be2d1e88d8dadd281560a65e8dc
Szobonya Gergő <31618132+djoke23@users.noreply.github.com>
2019-06-20 09:27:55 +0200

    URL-lek deklarálása.

--- project/protected/configs/admin_config.php
+++ project/protected/configs/admin_config.php
@@ -1,1 +1,9 @@
+define('ADMIN_URL', 'http://localhost/LibraryC/admin/');
+define('USERS_URL','http://localhost/LibraryC/users/');
+define('DEFAULT_URL','http://localhost/LibraryC/');
+define('ADMIN_PROTECTED','../protected/admin/');
+define('INTERFACES','../LibraryC/protected/interfaces/');
+define('MODULES',INTERFACES.'modules/');
+define('CHARSET','utf8');
 
+define('M_INTERFACES','../protected/interfaces/modules/');
