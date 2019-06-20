3428de6e7e3e6aef901fdaf3eff8afe9b3956561
Szobonya Gergő <31618132+djoke23@users.noreply.github.com>
2019-06-20 09:28:24 +0200

    Adatbázis rész hozzáadása.

--- project/protected/configs/admin_config.php
+++ project/protected/configs/admin_config.php
@@ -7,3 +7,10 @@ define('MODULES',INTERFACES.'modules/');
 define('CHARSET','utf8');
 
 define('M_INTERFACES','../protected/interfaces/modules/');
+/*Adatbázis rész:*/
+define('DB_TYPE','mysql');
+define('DB_NAME','db_test');
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','');
+define('DB_CHAR','utf8');
