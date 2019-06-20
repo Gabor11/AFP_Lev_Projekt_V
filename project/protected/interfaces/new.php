ac6b4fc7eb5611c024a7aac632c04167b46980c2
zsoltix83 <31615253+zsoltix83@users.noreply.github.com>
2019-06-20 09:26:13 +0200

    New tag megírása

--- project/protected/interfaces/new.php
+++ project/protected/interfaces/new.php
@@ -1,1 +1,16 @@
-
+<?php
+   if(array_key_exists("new_tag", $_POST)){
+       $query = "INSERT INTO users(userName,userPsswd) VALUES (\"".$_POST['userName']."\",\"".sha1($_POST['pw'])."\")";
+       executeDML("CALL Log('INSERT','users',".$_SESSION['uid'].")");
+       $success = executeDML($query);
+       if($success){
+           echo '<script>
+        alert(\'Sikerült\');
+    </script>';
+       }
+       else{
+           echo '<script>
+        alert(\'Hiba történt!\');
+    </script>';
+       }
+   }
